834b325cb5e3b31bb9285267e6adee9ac79b592e
Sergey <sergeyn213@gmail.com>
2024-07-16 22:20:54 +0300

    solution

--- src/DecimalComparator.java
+++ src/DecimalComparator.java
@@ -1,7 +1,10 @@
 public class DecimalComparator {
 
-    public static boolean areEqualByThreeDecimalPlaces{
+    public static boolean areEqualByThreeDecimalPlaces(double firstNumber, double secondNumber){
 
+        long firstIntNumber = (long) (firstNumber * 1000);
+        long secondIntNumber = (long) (secondNumber * 1000);
 
+        return firstIntNumber == secondIntNumber;
     }
 }
